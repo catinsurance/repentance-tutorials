90d2632d095942f7e640563c22d68ee9897678cd
Maya <mayacampa06@gmail.com>
2022-12-30 13:31:14 -0600

    episode 3

---
+++ mods/episode 3/main.lua
@@ -0,0 +1,32 @@
+local mod = RegisterMod("My Mod", 1)
+local damagePotion = Isaac.GetItemIdByName("Damage Potion")
+local damagePotionDamage = 1
+
+function mod:EvaluateCache(player, cacheFlags)
+    if cacheFlags & CacheFlag.CACHE_DAMAGE == CacheFlag.CACHE_DAMAGE then
+        local itemCount = player:GetCollectibleNum(damagePotion)
+        local damageToAdd = damagePotionDamage * itemCount
+        player.Damage = player.Damage + damageToAdd
+    end
+end
+
+mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.EvaluateCache)
+
+local bigRedButton = Isaac.GetItemIdByName("Big Red Button")
+
+function mod:RedButtonUse(item)
+    local roomEntities = Isaac.GetRoomEntities()
+    for _, entity in ipairs(roomEntities) do
+        if entity:IsActiveEnemy() and entity:IsVulnerableEnemy() then
+            entity:Kill()
+        end
+    end
+
+    return {
+        Discharge = true,
+        Remove = false,
+        ShowAnim = true
+    }
+end
+
+mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.RedButtonUse, bigRedButton)
